86341136da0f5a64ea866e33b9487049448a2dc0
Andrés SH <andres.sahe@gmail.com>
2020-09-19 18:47:19 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index-[chunkhash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React App',
+      filename: "index.html",
+      template: 'src/template.html',
+      favicon: 'src/favicon.ico'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
